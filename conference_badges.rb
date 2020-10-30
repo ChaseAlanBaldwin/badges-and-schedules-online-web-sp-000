3e9a6a429fbc7aaa41a1cf8a3fbce009bf516951
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-30 05:52:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(namesArray)
-
+  messages = []
+  x = 0
+  until x === namesArray.length + 1 do
+    x += 1
+    messages.push(badge_maker(namesArray[x]))
+  end
+  return messages
 end
+
+.each { |n| puts "Current number is: #{n}" }
