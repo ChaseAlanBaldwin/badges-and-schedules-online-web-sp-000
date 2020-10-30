45e01634f307ee510818da8328515e59c6cc3ca4
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-30 05:46:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,18 +1,4 @@
 # Write your code here.
 def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
-
-def batch_badge_creator(namesArray)
-  messages = []
-  x = 1
-  while x < namesArray.length do
-    x += 1
-    messages.push("Hello, my name is #{namesArray[x]}")
-  end
-  return messages
-end
-
-def assign_rooms(namesForRooms)
 
 end
